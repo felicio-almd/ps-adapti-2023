8e1cec0469cd6cb3f79106841658e0d543816c96
Felicio Almeida <feliciorar@gmail.com>
2023-11-23 01:55:30 -0300

    create alunos migration

---
+++ database/migrations/2023_11_23_014455_create_alunos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->foreignId('cursos_id')->constrained('cursos')->onDelete('cascade');
+            $table->$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos');
+    }
+};
