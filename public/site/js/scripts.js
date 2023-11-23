065de279ea5fb8311e03ce875d41ad6d80a3ba5b
Felicio Almeida <feliciorar@gmail.com>
2023-11-22 22:26:09 -0300

    Added a initial page style for project

---
+++ public/site/js/scripts.js
@@ -0,0 +1,66 @@
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// trocar o Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// carregar light ou dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode === "1") {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // save or remove dar mode
+    // localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    } else {
+        localStorage.removeItem("dark");
+    }
+});
+
+// mudar botao e salvar no localStorage
+function mudarTexto() {
+    var botao = document.getElementById("botao");
+
+    if (botao.innerHTML === "Contratar") {
+        // Se o texto atual for "Contratar", muda para "Contratado!"
+        botao.innerHTML = "Contratado!";
+        botao.classList.add("contratado");
+    } else {
+        botao.innerHTML = "Contratar";
+        botao.classList.remove("contratado");
+    }
+
+    // salvando no localStorage
+    localStorage.setItem("textoBotao", botao.innerHTML);
+}
+
+// carregar o texto do botão do localStorage ao carregar a página
+function carregarTexto() {
+    var botao = document.getElementById("botao");
+
+    // pega o texto do botão do localStorage
+    var textoSalvo = localStorage.getItem("textoBotao");
+
+    // se houver um texto salvo, define o texto do botão com esse valor
+    if (textoSalvo) {
+        botao.innerHTML = textoSalvo;
+        if (textoSalvo === "Contratado!") {
+            botao.classList.add("contratado");
+        }
+    }
+}
+
+// chama a função carregarTexto ao carregar a página
+window.onload = carregarTexto;
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,67 @@
 {{-- Front-end começa aqui! --}}
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="{{ asset('site/css/styles.css') }}">
+
+  <script src="https://code.iconify.design/3/3.1.1/iconify.min.js"></script>
+
+  <title>Seek Job</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="logo" role="banner">
+      <img class="logo-img" src="./public/assets/seek_job.png" alt="Logo da Seek Job" width="75" />
+    </div>
+
+    <div class="search" role="search"></div>
+
+    <section class="actions">
+      <div class="dark-mode" role="switch" aria-checked="false">
+        <input type="checkbox" name="change-theme" id="change-theme" />
+        <label for="change-theme">
+          <i class="sun iconify" data-icon="material-symbols:sunny" data-inline="false" aria-hidden="true"></i>
+          <i class="moon iconify" data-icon="material-symbols:mode-night" data-inline="false" aria-hidden="true"></i>
+        </label>
+      </div>
+      <a href="{{ route('login') }}" class="login" role="button" aria-label="Login">
+        <i class="iconify" data-icon="material-symbols:account-circle-full" data-inline="false" aria-hidden="true"></i>
+      </a>
+    </section>
+  </header>
+
+  <main role="main">
+    <section class="container">
+      <article class="card">
+        <img class="card-img-top" src="./public/assets/pinguin maloka.png" alt="Imagem de capa do card" />
+        <div class="infos">
+          <h3 class="name">Felicio</h3>
+          <p class="card-description">
+            Um exemplo de descrição rápida para construir o título do card e
+            fazer preencher o conteúdo do card.
+          </p>
+          <p class="occupation">Formado</p>
+          <p class="graduation">Ciencia da computação</p>
+          <button class="botao" id="botao" onclick="mudarTexto()">Contratar</button>
+        </div>
+      </article>
+    </section>
+  </main>
+
+  <footer class="footer" role="contentinfo">
+    <div class="footer__copy">
+      <p>Made with ❤ by @felicio.almd</p>
+      <p>Seek Job © All Rights Reserved</p>
+    </div>
+  </footer>
+  <script src="{{ asset('site/js/scripts.js') }}"></script>
+</body>
+
+</html>
