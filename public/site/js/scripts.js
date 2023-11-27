1ae5a0e87b7259b21d9763a94a1a2175e82f6007
Felicio Almeida <feliciorar@gmail.com>
2023-11-27 13:08:00 -0300

    remove contract button from scripts

--- public/site/js/scripts.js
+++ public/site/js/scripts.js
@@ -29,46 +29,6 @@ changeThemeBtn.addEventListener("change", function () {
     }
 });
 
-// mudar botao e salvar no localStorage
-// function mudarTexto() {
-//     var botao = document.getElementById("botao");
-
-//     if (botao.innerHTML === "Contratar") {
-//         // Se o texto atual for "Contratar", muda para "Contratado!"
-//         botao.innerHTML = "Contratado!";
-//         botao.classList.add("contratado");
-//     } else {
-//         botao.innerHTML = "Contratar";
-//         botao.classList.remove("contratado");
-//     }
-
-//     // salvando no localStorage
-//     localStorage.setItem("textoBotao", botao.innerHTML);
-// }
-
-// // carregar o texto do botão do localStorage ao carregar a página
-// function carregarTexto() {
-//     var botao = document.getElementById("botao");
-
-//     // pega o texto do botão do localStorage
-//     var textoSalvo = localStorage.getItem("textoBotao");
-
-//     // se houver um texto salvo, define o texto do botão com esse valor
-//     if (textoSalvo) {
-//         botao.innerHTML = textoSalvo;
-//         if (textoSalvo === "Contratado!") {
-//             botao.classList.add("contratado");
-//         }
-//     }
-// }
-
-// // chama a função carregarTexto ao carregar a página
-// window.onload = carregarTexto;
-
-// $(document).ready(function () {
-//     $("html,body").animate({ scrollTop: document.body.scrollHeight }, "slow");
-// });
-
 let mybutton = document.getElementById("scroll-button");
 
 // mostra o botao ao usuario scrollar qualquer quantia
@@ -97,56 +57,56 @@ function toTop() {
 }
 
 //
-// Funcao troca botao contratar/contratado
-//
-function verifyKey(value) {
-    // verifica se o valor existe dentro da chave e se ela existe
-    const keys = localStorage.getItem("contratados");
-    if (keys && keys.includes(value)) {
-        return true;
-    }
-    return false;
-}
+// Funcao troca botao contratar/contratado  -- USANDO APENAS JAVASCRIPT COM O LOCALSTORAGE
+// //
+// function verifyKey(value) {
+//     // verifica se o valor existe dentro da chave e se ela existe
+//     const keys = localStorage.getItem("contratados");
+//     if (keys && keys.includes(value)) {
+//         return true;
+//     }
+//     return false;
+// }
 
-function setupButtons() {
-    // Configura o estado inicial dos botoes ao recarregar a pagina com base nos dados salvos no localStorage
-    const buttons = document.querySelectorAll(".botao");
-    buttons.forEach((button) => {
-        const value = button.value;
-        const isContratado = verifyKey(value);
+// function setupButtons() {
+//     // Configura o estado inicial dos botoes ao recarregar a pagina com base nos dados salvos no localStorage
+//     const buttons = document.querySelectorAll(".botao");
+//     buttons.forEach((button) => {
+//         const value = button.value;
+//         const isContratado = verifyKey(value);
 
-        if (isContratado) {
-            button.textContent = "Contratado!";
-            button.classList.add("contratado");
-        } else {
-            button.textContent = "Contratar";
-            button.classList.remove("contratado");
-        }
-    });
-}
+//         if (isContratado) {
+//             button.textContent = "Contratado!";
+//             button.classList.add("contratado");
+//         } else {
+//             button.textContent = "Contratar";
+//             button.classList.remove("contratado");
+//         }
+//     });
+// }
 
-function saveItem(value, buttonId) {
-    // Salva o valor de cada id dos botões na chave e faz a troca de texto em tempo real
-    const existKey = verifyKey(value);
-    let dataArray = JSON.parse(localStorage.getItem("contratados")) || [];
-    if (existKey) {
-        let index = dataArray.indexOf(value);
-        dataArray.splice(index, 1);
-    } else {
-        dataArray.push(value);
-    }
-    localStorage.setItem("contratados", JSON.stringify(dataArray));
+// function saveItem(value, buttonId) {
+//     // Salva o valor de cada id dos botões na chave e faz a troca de texto em tempo real
+//     const existKey = verifyKey(value);
+//     let dataArray = JSON.parse(localStorage.getItem("contratados")) || [];
+//     if (existKey) {
+//         let index = dataArray.indexOf(value);
+//         dataArray.splice(index, 1);
+//     } else {
+//         dataArray.push(value);
+//     }
+//     localStorage.setItem("contratados", JSON.stringify(dataArray));
 
-    const cardButton = document.getElementById(buttonId);
+//     const cardButton = document.getElementById(buttonId);
 
-    if (existKey) {
-        cardButton.textContent = "Contratar";
-        cardButton.classList.remove("contratado");
-    } else {
-        cardButton.textContent = "Contratado!";
-        cardButton.classList.add("contratado");
-    }
-}
+//     if (existKey) {
+//         cardButton.textContent = "Contratar";
+//         cardButton.classList.remove("contratado");
+//     } else {
+//         cardButton.textContent = "Contratado!";
+//         cardButton.classList.add("contratado");
+//     }
+// }
 
-// chamada da função de verificar estado inicial dos botões toda vez que a pagina é carregada
-window.addEventListener("load", setupButtons);
+// // chamada da função de verificar estado inicial dos botões toda vez que a pagina é carregada
+// window.addEventListener("load", setupButtons);
